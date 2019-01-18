001f5351f18eb5b0635d8061ec3c3e88845ee239
Daniel Stefan Klose <daniel-stefan.klose@udo.edu>
2019-01-18 11:03:30 +0100

    added interview Create

---
+++ src/resolvers/Mutations/interviewMutations.js
@@ -0,0 +1,50 @@
+const Joi = require('joi');
+const bcrypt = require('bcryptjs');
+const moment = require('moment');
+
+const template = {
+  async createTemplate(parent, args, ctx, info) {
+    if (!ctx.user) {
+      throw new Error('not authenticated');
+    }
+    if (!ctx.user.company) {
+      throw new Error('No Company found');
+    }
+
+    const from = moment(Date.now());
+    const to = moment(args.data.activeUntil);
+
+    const price = (args.data.limit * to.diff(from, 'days')) / 10;
+    const newCredits = ctx.user.company.credits - price;
+
+    if (newCredits < 0) {
+      throw new Error('Not enough credits');
+    }
+
+    const interview = await ctx.db.mutation.createInterview(
+      {
+        data: {
+          ...args.data,
+          creator: {
+            connect: {
+              id: ctx.user.id,
+            },
+          },
+          company: {
+            connect: {
+              id: ctx.user.company.id,
+            },
+          },
+        },
+      },
+      info,
+    );
+
+    await ctx.db.mutation.updateCompany(
+      { data: { credits: newCredits } },
+      { where: { id: ctx.user.company.id } },
+    );
+  },
+};
+
+module.exports = template;
