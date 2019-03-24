157e388737a530796211f133f7b03d27b805d6ce
Daniel Stefan Klose <daniel-stefan.klose@udo.edu>
2019-03-24 14:05:30 +0100

    added result query

--- src/resolvers/Queries/interviewQueries.js
+++ src/resolvers/Queries/interviewQueries.js
@@ -66,6 +66,73 @@ const interview = {
       info,
     );
   },
+
+  async myResults(parent, args, ctx, info) {
+    if (!ctx.user) {
+      throw new Error('not authenticated');
+    }
+
+    if (!ctx.user.company) {
+      throw new Error('No Company found');
+    }
+
+    const results = await ctx.db.query.results(
+      {
+        ...args,
+        where: { ...args.where, company: { id: ctx.user.company.id } },
+      },
+      info,
+    );
+    return results;
+  },
+
+  async myResultsConnection(parent, args, ctx, info) {
+    if (!ctx.user) {
+      throw new Error('not authenticated');
+    }
+
+    if (!ctx.user.company) {
+      throw new Error('No Company found');
+    }
+
+    const resultCon = await ctx.db.query.resultsConnection(
+      {
+        ...args,
+        where: { ...args.where, company: { id: ctx.user.company.id } },
+      },
+      info,
+    );
+
+    return resultCon;
+  },
+
+  async result(parent, args, ctx, info) {
+    if (!ctx.user) {
+      throw new Error('not authenticated');
+    }
+
+    if (!ctx.user.company) {
+      throw new Error('No Company found');
+    }
+
+    const resulttemp = await ctx.db.query.result(
+      {
+        where: { id: args.id },
+      },
+      '{company {id}}',
+    );
+
+    if (resulttemp.company.id !== ctx.user.company.id) {
+      throw new Error('not authorised');
+    }
+
+    return await ctx.db.query.result(
+      {
+        where: { id: args.id },
+      },
+      info,
+    );
+  },
 };
 
 module.exports = interview;
